8808b462b257beffec1abe3ddd59a6c26107b993
Administrator <shasikanth.shau@outlook.com>
2016-10-28 16:35:14 -0700

    2nd Push

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// This is message from the dev team!!!
 		System.out.println("Hello Happy Sales People!");
 		System.out.println("This app shows sales data.");
 	}
