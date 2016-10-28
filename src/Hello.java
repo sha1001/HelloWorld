97d0905b3bd1b61bb71b032b124a2d25415c1750
Administrator <shasikanth.shau@outlook.com>
2016-10-28 16:23:04 -0700

    First push

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+import java.io.Console;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Happy Sales People!");
+		System.out.println("This app shows sales data.");
+	}
+
+}
